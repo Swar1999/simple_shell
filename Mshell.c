a9c827ddb724d715886dd551edf186c4d0f0e463
swar1999 <muzan22kris@gmail.com>
2023-11-10 06:44:28 -0800

    task 1

--- Mshell.c
+++ Mshell.c
@@ -7,24 +7,23 @@
  */
 int main(int argc, char *argv[])
 {
-	if (isatty(STDIN_FILENO));
-	char *prompt = "(Myshell) $";/*creat new prompt */
+	char *prompt = "$ ";/*creat new prompt */
 	char *read_line = NULL, *copy_line = NULL/*getline function paramter*/
 	size_t n = 0;
 	ssize_t num_line;/*return size of bytes or -1*/
-	const char *dlimiter = " \n";/*spareates the string commpnands*/
 	int token_count = 0;
 	char *token;
 	int i;
 		(void)argc;/*ignor the paramter argc*/
 		while(1)
 		{
+			if (isatty(STDIN_FILENO));
 			printf("%s", prompt);
 			num_line = getline(&read_line, &n, stdin);/*read the entier stream of string*/
 			if (num_line == -1)
 			{
-				printf("error...\n");
-				return(-1);
+				printf("\n");/*print a new line*/
+				break;/*end*/
 			}
 			copy_line = malloc(sizeof(char) * num_line);
 			if (copy_line == NULL)/*malloc faild*/
@@ -33,14 +32,16 @@ int main(int argc, char *argv[])
 				return(-1);
 			}
 			_strcyp(copy_line, read_line);/*save copy before break into tokens*/
-			token = strtok(read_line, dlimiter);/*read string and spreated with dlimiter*/
+			token = strtok(read_line, " \n\t");/*read string and spreated with dlimiter*/
 			while (token != NULL)
 			{
 				token_count++;
-				token = strtok(NULL, d limiter);
+				token = strtok(NULL, " \n\t");
 			}
 			token_count++;
-			argv = malloc(sizeof 
+			argv = malloc(sizeof(char *) *token_count);
+			token = strtok(copy_line, " \n\t");/*store each token*/
+
 			printf("%s\n", read_line);
 			free(read_line);
 		}
--- help_func.c
+++ help_func.c
@@ -6,13 +6,29 @@
  * @src: pointer
  * Return: void
  */
-char *_strcpy(char *dest, char *src)
+void _strcpy(char *dest, char *src)
 {
-        int j;
-                j = 0;
-                while (j < n && src[j] != '\0')
+        int j = 0;
+		while ( src[j] != '\0')
+		{
+			dest[j] = src[j];
+			j++;
+		}
+		dest[j] = '\0';/*last element in string*/
+}
+/**
+ * _strlen - Entry point
+ * Description: program that print the string length
+ * @s: pointer
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+        int len = 0;
+                while (*s != '\0')
                 {
-                        dest[j] = src[j];
-                        j++;
+                        len++;
+                        s++;
                 }
+                return (len);
 }
